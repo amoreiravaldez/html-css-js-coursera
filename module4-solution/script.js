db688dcebe5819b21a1a5cbc6b7ee7de0ea95683
Alvaro Gabriel Moreira Valdez <amoreiravaldez@gmail.com>
2020-07-19 19:12:22 -0300

    "New changes"

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -39,7 +39,7 @@ WARNING!!! WARNING!!!
     // Loop over the names array and say either 'Hello' or "Good Bye"
     // using the 'speak' method or either helloSpeaker's or byeSpeaker's
     // 'speak' method.
-    // See Lecture 50, part 1
+    // See Lecture 50, part 1 
     for (var i = 0; i < names.length; i++) {
     
       // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
